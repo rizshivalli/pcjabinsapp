10427ccef86591d7e98ab095a07f11f50fca39a8
Rizwan Shivalli <rizwan.a@simplificommerce.com>
2020-02-15 14:32:16 +0530

    HomeScreen Changes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,78 +1,64 @@
-// import React, { useState } from "react";
-// import { View, StyleSheet, Text, Button, Alert } from "react-native";
-// import { TextInput } from "react-native-paper";
+import React, { useState } from "react";
+import { View, StyleSheet, Text, Button, Alert } from "react-native";
+import { TextInput } from "react-native-paper";
 
-// const HomeScreen = ({ navigation }) => {
-//   const [email, setEmail] = useState(""),
-//     [password, setPassword] = useState("");
+const HomeScreen = ({ navigation }) => {
+  const [email, setEmail] = useState(""),
+    [password, setPassword] = useState("");
 
-//   const validateUser = () => {
-//     console.log(email);
-//     if (!email.includes("@")) {
-//       Alert.alert("Please enter a valid email");
-//     } else if (password.length < 6) {
-//       Alert.alert("Please enter a valid Password");
-//     } else {
-//       navigation.navigate("Profile");
-//     }
-//   };
+  const validateUser = () => {
+    console.log(email);
+    if (!email.includes("@")) {
+      Alert.alert("Please enter a valid email");
+    } else if (password.length < 6) {
+      Alert.alert("Please enter a valid Password");
+    } else {
+      navigation.navigate("Profile");
+    }
+  };
 
-//   return (
-//     <View style={styles.container}>
-//       <View style={styles.childContainer}>
-//         <TextInput
-//           placeholder="Please Enter Your Email"
-//           mode="outlined"
-//           label="Email"
-//           value={email}
-//           onChangeText={text => {
-//             setEmail(text);
-//           }}
-//         />
-//       </View>
-//       <View style={styles.childContainer}>
-//         <TextInput
-//           placeholder="Please Enter Your Password"
-//           mode="outlined"
-//           label="Password"
-//           value={password}
-//           onChangeText={text => {
-//             setPassword(text);
-//           }}
-//           secureTextEntry={true}
-//         />
-//       </View>
-//       <View style={styles.childContainer}>
-//         <Button
-//           title="go to profileScreen "
-//           onPress={() => {
-//             validateUser();
-//           }}
-//         />
-//       </View>
-//     </View>
-//   );
-// };
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1
-//   },
-//   childContainer: {
-//     margin: "5%"
-//   }
-// });
-// export default HomeScreen;
-
-import React from "react";
-
-import { View } from "react-native";
-
-const HomeScreen = () => {
   return (
-    <View style={{ flex: 1, backgroundColor: "red" }}>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <View style={styles.childContainer}>
+        <TextInput
+          placeholder="Please Enter Your Email"
+          mode="outlined"
+          label="Email"
+          value={email}
+          onChangeText={text => {
+            setEmail(text);
+          }}
+        />
+      </View>
+      <View style={styles.childContainer}>
+        <TextInput
+          placeholder="Please Enter Your Password"
+          mode="outlined"
+          label="Password"
+          value={password}
+          onChangeText={text => {
+            setPassword(text);
+          }}
+          secureTextEntry={true}
+        />
+      </View>
+      <View style={styles.childContainer}>
+        <Button
+          title="go to profileScreen "
+          onPress={() => {
+            validateUser();
+          }}
+        />
+      </View>
     </View>
   );
 };
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  childContainer: {
+    margin: "5%"
+  }
+});
 export default HomeScreen;
